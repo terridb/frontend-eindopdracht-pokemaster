641f11aaab930afa28aa2d95d459e43ac788de18
terridb <terrideboer99@gmail.com>
2025-04-12 16:11:06 +0200

    Opmaak login & registratie pagina

--- src/pages/authentication/login/LogIn.jsx
+++ src/pages/authentication/login/LogIn.jsx
@@ -1,8 +1,59 @@
-function LogIn(props) {
+import "LogIn.css";
+import {Link} from "react-router-dom";
+import greyLogo from "../../../assets/logo/logo-grey.png";
+import GeneralButton from "../../../components/general-button/GeneralButton.jsx";
+import snorlax from "../../../assets/images/snorlax.png";
+
+function LogIn() {
     return (
-        <>
-            <h1>Login</h1>
-        </>
+        <main>
+            <section className="outer-container">
+                <section className="auth-content">
+                    <Link to="/">
+                        <img
+                            src={greyLogo}
+                            alt="Pokémaster logo"
+                            className="auth-logo"
+                        />
+                    </Link>
+                    <h1>Log in to Pokémaster</h1>
+                    <form className="auth-form">
+                        <label htmlFor="email-field">E-mailaddress
+                            <input
+                                type="email"
+                                id="email-field"
+                                name="email"
+                                placeholder="E-mailaddress"
+                            />
+                        </label>
+                        <label htmlFor="password-field">Password
+                            <input
+                                type="password"
+                                id="password-field"
+                                name="password"
+                                placeholder="Password"
+                            />
+                        </label>
+                        <GeneralButton
+                            pokemonName="snorlax"
+                            buttonType="submit"
+                            buttonText="sign-in"
+                        />
+                    </form>
+                    <p>
+                       Need an account?
+                        <Link to="/signup" className="auth-link"> Register here</Link>
+                    </p>
+                </section>
+                <figure className="auth-background-container">
+                    <img
+                        className="auth-background"
+                        src={snorlax}
+                        alt="Snorlax"
+                    />
+                </figure>
+            </section>
+        </main>
     );
 }
 
--- src/pages/authentication/registration/Registration.jsx
+++ src/pages/authentication/registration/Registration.jsx
@@ -1,8 +1,75 @@
-function Registration(props) {
+import "./Registration.css";
+import greyLogo from "../../../assets/logo/logo-grey.png";
+import snorlax from "../../../assets/images/snorlax.png";
+import {Link} from "react-router-dom";
+import GeneralButton from "../../../components/general-button/GeneralButton.jsx";
+
+function Registration() {
     return (
-        <>
-            <h1>Registratie</h1>
-        </>
+        <main>
+            <section className="outer-container">
+                <section className="auth-content">
+                    <Link to="/">
+                        <img
+                            src={greyLogo}
+                            alt="Pokémaster logo"
+                            className="auth-logo"
+                        />
+                    </Link>
+                    <h1>Join Pokémaster</h1>
+                    <form className="auth-form">
+                        <label htmlFor="email-field">E-mailaddress
+                            <input
+                                type="email"
+                                id="email-field"
+                                name="email"
+                                placeholder="E-mailaddress"
+                            />
+                        </label>
+                        <label htmlFor="password-field">Password
+                            <input
+                                type="password"
+                                id="password-field"
+                                name="password"
+                                placeholder="Password"
+                            />
+                        </label>
+                        <label htmlFor="password-check-field">Confirm Password
+                            <input
+                                type="password"
+                                id="password-check-field"
+                                name="password-check"
+                                placeholder="Password"
+                            />
+                        </label>
+                        <label htmlFor="username-field">Username
+                            <input
+                                type="text"
+                                id="username-field"
+                                name="username"
+                                placeholder="Username"
+                            />
+                        </label>
+                        <GeneralButton
+                            pokemonName="snorlax"
+                            buttonType="submit"
+                            buttonText="continue"
+                        />
+                    </form>
+                    <p>
+                        Already have an account?
+                        <Link to="/login" className="auth-link"> Sign in here</Link>
+                    </p>
+                </section>
+                <figure className="auth-background-container">
+                    <img
+                        className="auth-background"
+                        src={snorlax}
+                        alt="Snorlax"
+                    />
+                </figure>
+            </section>
+        </main>
     );
 }
 
